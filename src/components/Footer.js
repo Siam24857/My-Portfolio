9c85fb94ad68093cfb65177f0d853c0ca9b3c020
Siam24857 <siamtechofficial1597@gmail.com>
2026-06-03 00:30:00 +0600

    all problems slove

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,10 +18,12 @@ export default function Footer() {
         </div>
         <p className="text-sm text-text-muted mb-10">Designed with love, all right reserved for Sheikh Siam.</p>
         <div className="flex justify-center space-x-6">
-          <a className="p-3 bg-dark-card border border-white/10 rounded-full hover:border-coral-accent transition-colors" href="siamtechofficial1597@gmail.com">
+          <a  href="mailto:siamtechofficial1597@gmail.com">
+          <div className="p-3 bg-dark-card border border-white/10 rounded-full hover:border-coral-accent transition-colors">
             <svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 24 24">
               <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"></path>
             </svg>
+          </div>
           </a>
           <a className="p-3 bg-dark-card border border-white/10 rounded-full hover:border-coral-accent transition-colors" href="https://github.com/Siam24857">
             <svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 24 24">
